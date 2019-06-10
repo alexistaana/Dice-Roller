1ddc15b64cfaf524ac6e82416616c97eacea5aec
Marco Alexi Sta Ana <alexistaana@yahoo.com.ph>
2019-06-10 00:22:19 -0700

    added barebones

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//RANDOMLY GENERATES A NUMBER AND CONVERTS TO DICE ROLL
+function rollDice(){
+    var dice = Math.floor((Math.random() * 6) + 1)
+
+    return dice;
+}
+
+function drawDice(){
+    
+}
